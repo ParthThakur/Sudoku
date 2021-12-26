e4f04d32b978d1ae9df8305459f45043ab41b8b6
ParthThakur <parththakur@gmail.com>
2021-12-26 19:34:44 +0000

    Create a board class

--- solver.py
+++ solver.py
@@ -1,6 +1,24 @@
-def solve(board):
-    if len(board) != 81:
+import re
+
+
+class Board:
+
+    def __init__(self, initial) -> None:
+        rows = re.findall('.{9}', initial)
+        self.board = [list(row) for row in rows]
+
+    def getSolution(self) -> str:
+        solution = [''.join(row) for row in self.board]
+        return ''.join(solution)
+
+
+def solve(initial):
+    if len(initial) != 81:
         raise ValueError('Invalid Board. Must contain 81 values.')
-    
-    solution = board
-    return solution
+
+    board = Board(initial)
+    return board.getSolution()
+
+if __name__ == '__main__':
+    initial_values = input('Enter initial values: ')
+    solve(initial_values)
